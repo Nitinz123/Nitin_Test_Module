16aa64ba5b63a91394c6896d26c61510aae95d05
nitinz123 <iacecomtesting@gmail.com>
2022-01-14 12:56:22 +0530

    Adding the Project

---
+++ src/main/java/seleniumgrpid/seleniumartid/TestClass.java
@@ -0,0 +1,11 @@
+package seleniumgrpid.seleniumartid;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Working");
+
+	}
+
+}
